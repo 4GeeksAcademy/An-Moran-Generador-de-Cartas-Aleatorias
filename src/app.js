35bf4dbfc733da6cc2d487619eeddddda4fa3096
Aneth Morantes <84743013+An-moran@users.noreply.github.com>
2025-12-06 21:17:26 +0000

    primer commit random cards

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+const palos = ["♦", "♥", "♣", "♠"];
+
+const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+const palo = palos[Math.floor(Math.random() * palos.length)];
+
+const numero = numeros[Math.floor(Math.random() * numeros.length)];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+document.getElementById("paloTop").innerHTML = palo;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById("paloBottom").innerHTML = palo;
+
+document.getElementById("numeroCentro").innerHTML = numero;
+
+const card = document.getElementById("card");
+
+card.classList.remove("spade", "club", "heart", "diamond");
+
+if (palo === "♠") card.classList.add("spade");
+
+if (palo === "♣") card.classList.add("club");
+
+if (palo === "♥") card.classList.add("heart");
+
+if (palo === "♦") card.classList.add("diamond");
+
